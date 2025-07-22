cf4f0808cb2631ccd075365a949ac256d1a3fac9
Henrique Ribeiro <henrique.ribeiro@stellantis.com>
2025-07-22 14:50:52 -0300

    Data feed euristics

--- backtestament/data/data_loader.py
+++ backtestament/data/data_loader.py
@@ -7,38 +7,77 @@ DATE_COL = 'DATE'
 TIME_COL = 'TIME'
 DATE_TIME_COL = 'DATE_TIME'
 
-class DataLoader:
-    def __init__(self, assets: List[str], path: str, start: datetime, end: datetime):
-        self.assets = assets
-        self.path = path[:-1] if path[-1] == '/' else path
-        self.start = start
-        self.end = end
 
-    def _merge_date_time_cols(self, frame: pd.DataFrame) -> None:
+class _DataFeed:
+    def __init__(self, df: pd.DataFrame, start: datetime, end: datetime):
+        self._next_row_index = 0
+        self._data_finished = False
+        df = self._merge_date_time_cols(df)
+        min_time = pd.Timestamp(start)
+        max_time = pd.Timestamp(end)
+        mask = (df[DATE_TIME_COL] >= max_time) & (df[DATE_TIME_COL] <= min_time)
+        df = df.loc[mask, :]
+        print(df[DATE_TIME_COL])
+        self._df = df
+
+    def data_finished(self) -> bool:
+        return self._data_finished
+
+    def get_next_row(self) -> pd.Series:
+        ret = self._df.iloc[self._next_row_index, :]
+        self._next_row_index += 1
+        if self._next_row_index >= self._df.shape[0]:
+            self._data_finished = True
+        return ret
+
+    def get_next_row_timestamp(self) -> pd.Timestamp:
+        return self._df.iloc[self._next_row_index][DATE_TIME_COL]
+
+    def _merge_date_time_cols(self, frame: pd.DataFrame) -> pd.DataFrame:
         merge_date_time_str = frame[DATE_COL] + ' ' + frame[TIME_COL]
         merge_date_time = pd.to_datetime(merge_date_time_str)
         frame[DATE_TIME_COL] = merge_date_time
-        frame.drop([DATE_COL, TIME_COL], axis=1)
+        frame.drop([DATE_COL, TIME_COL], axis=1, inplace=True)
+        return frame
+
+    def get_max_time(self) -> pd.Timestamp:
+        return self._df.iloc[-1][DATE_TIME_COL]
+
+    def get_min_time(self) -> pd.Timestamp:
+        return self._df.iloc[0][DATE_TIME_COL]
+
+
+class DataLoader:
+    def __init__(self, assets: List[str], path: str, start: datetime, end: datetime):
+        self._assets = assets
+        self._path = path[:-1] if path[-1] == '/' else path
+        self._start = start
+        self._end = end
 
     def load(self) -> None:
-        self.frames = {asset: pd.read_csv(f"{self.path}/{asset}.csv") for asset in self.assets}
-        for asset in self.frames.keys():
-            frame = self.frames[asset]
-            self._merge_date_time_cols(frame)
-            max_time = pd.Timestamp(self.start)
-            min_time = pd.Timestamp(self.end)
-            mask = (frame[DATE_TIME_COL] >= max_time) & (frame[DATE_TIME_COL] <= min_time)
-            frame = frame.loc[mask, :]
-            frame.index = frame[DATE_TIME_COL]
-            self.frames[asset] = frame
+        self._frames = {asset: _DataFeed(pd.read_csv(f"{self._path}/{asset}.csv"),
+                                         self._start, self._end) for asset in self._assets}
+        self._last_data_timestamp = pd.Timestamp(0)
+
+    def get_next_data(self) -> tuple[str, pd.Series]:
+        time_diff = pd.Timedelta.max
+        ret = ("", pd.Series())
+        for asset, feed in self._frames.items():
+            if feed.data_finished():
+                continue
+            diff = feed.get_next_row_timestamp() - self._last_data_timestamp
+            if diff >= time_diff:
+                continue
+            time_diff = diff
+            ret = (asset, feed.get_next_row())
+            self._last_data_timestamp
+        return ret
 
     def get_num_frames(self) -> int:
-        return len(self.frames)
+        return len(self._frames)
 
     def get_max_time(self, asset: str) -> datetime:
-        idx = self.frames[asset].index[-1]
-        return idx.to_pydatetime()
+        return self._frames[asset].get_max_time().to_pydatetime()
 
     def get_min_time(self, asset: str) -> datetime:
-        idx = self.frames[asset].index[0]
-        return idx.to_pydatetime()
+        return self._frames[asset].get_min_time().to_pydatetime()
--- tests/test_datalodaer.py
+++ tests/test_datalodaer.py
@@ -2,7 +2,8 @@ import sys
 import pytest
 from datetime import datetime, timedelta
 
-from backtestament.data.data_loader import DataLoader
+from backtestament.data.data_loader import DataLoader, _DataFeed
+
 
 @pytest.fixture
 def single_asset() -> DataLoader:
@@ -14,6 +15,7 @@ def single_asset() -> DataLoader:
     return dl
 
 
+
 def test_load_success_single_asset(single_asset: DataLoader) -> None:
     single_asset.load()
     assert 1 == single_asset.get_num_frames()
@@ -23,6 +25,7 @@ def test_load_success_single_asset(single_asset: DataLoader) -> None:
     time_delta_expect: timedelta = date_end - date_start
     time_delta: timedelta = single_asset.get_max_time('WIN') - single_asset.get_min_time('WIN')
     assert abs((time_delta - time_delta_expect).total_seconds()) < (10 * 60)
+
 
 if __name__ == '__main__':
     sys.exit(pytest.main(__file__))
